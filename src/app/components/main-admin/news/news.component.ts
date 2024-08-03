8058a42e83e84d0f374ed470a81a35a3f88e118b
Nguyễn Thanh Tùng <nttung2k3k@gmail.com>
2024-08-03 22:12:50 +0700

    Done tinyMCE integration and firebase to upload image

--- src/app/components/main-admin/news/news.component.ts
+++ src/app/components/main-admin/news/news.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../../core/data.service';
 import { CommonVariable } from '../../../common/common.variable';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
+import { NotificationService } from '../../../core/notification.service';
+import { EditorComponent } from '@tinymce/tinymce-angular';
+import { finalize, last } from 'rxjs/operators';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Component({
   selector: 'app-news',
@@ -14,9 +20,52 @@ export class NewsComponent implements OnInit {
   totalNews: number = -1;
   keyword: string = "";
   newsData: any = [];
+  createUpdateForm: FormGroup = new FormGroup({
+    NewName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(70)]),
+    Title: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(70)]),
+    Image: new FormControl(null, [Validators.required]),
+    Description: new FormControl(''),
+    Status: new FormControl('', [Validators.required]),
+  });
+  source: string = '';
+  title = 'images-tinymce';
+  config: EditorComponent['init'] = {
+    base_url: "/tinymce",
+    suffix: ".min",
+    plugins: "lists link image table wordcount",
+    toolbar: "undo redo | formatselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image",
+    automatic_uploads: true,
+
+    images_upload_handler: (blobInfo) => {
+      const file = blobInfo.blob();
+      const filePath = `${Date.now()}-${blobInfo.filename()}`;
+      const ref = this.storage.ref(filePath);
+      const task = this.storage.upload(filePath, file);
+      const promise = new Promise<string>((resolve, reject) => {
+        task
+          .snapshotChanges()
+          .pipe(
+            finalize(() =>
+              ref
+                .getDownloadURL()
+                .pipe(last())
+                .subscribe((url: any) => {
+                  resolve(url);
+                })
+            )
+          )
+          .subscribe((_) => {
+            // do nothing
+          });
+      });
+      return promise;
+    },
+  };
 
 
-  constructor(private _dataService: DataService) { }
+
+
+  constructor(private _dataService: DataService, private _notificationService: NotificationService, private storage: AngularFireStorage) { }
 
 
   ngOnInit(): void {
@@ -24,7 +73,6 @@ export class NewsComponent implements OnInit {
   }
   public LoadData() {
     this._dataService.get(`/api/News/View?Keyword=${this.keyword}&pageIndex=${this.pageIndex}`).subscribe(response => {
-      console.log(response);
       this.newsData = response.resultObj.items;
       this.pageIndex = response.resultObj.pageIndex;
       this.pageSize = response.resultObj.pageSize;
@@ -34,6 +82,33 @@ export class NewsComponent implements OnInit {
   onPageIndexChange(pageIndex: number) {
     this.pageIndex = pageIndex;
     this.LoadData();
+  }
+
+  // Handle Create/Update
+  isVisible = false;
+
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+  handleChange(info: NzUploadChangeParam): void {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      this._notificationService.showSuccess('Success', `${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      this._notificationService.showError('Error', `${info.file.name} file upload failed.`);
+    }
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+  onCreateUpdate() {
+    this.isVisible = false;
+
+
   }
 
 }
--- src/app/components/main-admin/news/news.module.ts
+++ src/app/components/main-admin/news/news.module.ts
@@ -4,15 +4,24 @@ import { DataService } from '../../../core/data.service';
 import { NzPaginationModule } from 'ng-zorro-antd/pagination';
 import { NewsComponent } from './news.component';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { FirebaseEnvirontment } from '../../../common/firebase.environment';
 
 
 
 @NgModule({
   declarations: [NewsComponent],
   imports: [
-    CommonModule, NzPaginationModule, RouterModule, FormsModule
+    CommonModule, NzPaginationModule, RouterModule, FormsModule, NzModalModule, ReactiveFormsModule, NzSwitchModule
+    , NzUploadModule, EditorModule, AngularFireModule.initializeApp(FirebaseEnvirontment.environment.firebase),
+    AngularFireStorageModule,
   ],
-  providers: [DataService],
+  providers: [DataService, { provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' }],
 })
 export class NewsModule { }
